fa9c088ac8cf576a3227cddc149ebc51d23dd282
Jakub Pietrzyk <jakub.pietrzyk01@gmail.com>
2025-02-06 23:59:39 +0100

    Map page half made - globe

---
+++ scripts/globe.js
@@ -0,0 +1,88 @@
+let autoRotate = true;
+let timer, projection, svg;
+const sensitivity = 40;
+
+
+async function drawGlobe() {
+    const response = await fetch('../data/c.json');
+    const world_json = await response.json();
+
+    const countriesResponse = await fetch('../data/countries.json');
+    const countries = await countriesResponse.json();
+
+    const isMobile = window.innerWidth < 768;
+
+    const width = window.innerWidth;
+    const height = window.innerHeight - 80;
+    const size = isMobile ? 130 : 250;
+
+    projection = d3.geoOrthographic()
+        .scale(size)
+        .center([0, 0])
+        .rotate([0, -30])
+        .translate([width / 2, height / 2]);
+
+    const initialScale = projection.scale();
+    let path = d3.geoPath().projection(projection);
+
+    svg = d3.select("#globe")
+        .attr("width", width)
+        .attr("height", height);
+
+    let globe = svg.append("circle")
+        .attr("fill", "#EEE")
+        .attr("stroke", "#000")
+        .attr("stroke-width", "0.2")
+        .attr("cx", width / 2)
+        .attr("cy", height / 2)
+        .attr("r", initialScale);
+
+    svg.call(d3.drag().on('drag', (event) => {
+        const rotate = projection.rotate();
+        const k = sensitivity / projection.scale();
+        projection.rotate([
+            rotate[0] + event.dx * k,
+            rotate[1] - event.dy * k
+        ]);
+        path = d3.geoPath().projection(projection);
+        svg.selectAll("path").attr("d", path);
+    }));
+
+    let map = svg.append("g");
+
+    map.append("g")
+        .attr("class", "countries")
+        .selectAll("path")
+        .data(world_json.features)
+        .enter().append("path")
+        .attr("class", "country")
+        .attr("d", path)
+        .style("fill", (d) => {
+            if(d.properties.name === "Poland") {
+                return "gold";
+            } else if (countries.countries.includes(d.properties.name)) {
+                return "red";
+            } else {
+                return "#fff";
+            }
+        })
+        .style('stroke', 'black')
+        .style('stroke-width', 0.3)
+        .style("opacity", 0.8);
+
+    // Optional rotate
+    timer = d3.timer(function(elapsed) {
+        if (autoRotate) {
+            const rotate = projection.rotate();
+            const k = sensitivity / projection.scale();
+            projection.rotate([
+                rotate[0] - 1 * k,
+                rotate[1]
+            ]);
+            path = d3.geoPath().projection(projection);
+            svg.selectAll("path").attr("d", path);
+        }
+    }, 200);
+}
+
+drawGlobe();
---
+++ scripts/map_nav.js
@@ -0,0 +1,50 @@
+const ready = function() {
+    const globeLink = document.getElementById("globe_link");
+    const countriesLink = document.getElementById("countries_link");
+    const pinsLink = document.getElementById("pins_link");
+
+    const globeCont = document.getElementById("globe_cont");
+    const countriesCont = document.getElementById("countries_cont");
+    const pinsCont = document.getElementById("pins_cont");
+
+    const wrapper = document.getElementById("map_content_wrapper");
+    const screenHeight = window.innerHeight;
+
+    globeLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        if (globeLink.classList.contains("active")) return;
+
+        wrapper.style.top = `0px`;
+        countriesLink.classList.remove("active");
+        pinsLink.classList.remove("active");
+        globeLink.classList.add("active");
+    });
+
+    countriesLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        if (countriesLink.classList.contains("active")) return;
+
+        wrapper.style.top = `-${screenHeight}px`;
+        globeLink.classList.remove("active");
+        pinsLink.classList.remove("active");
+        countriesLink.classList.add("active");
+    });
+
+    pinsLink.addEventListener("click", (e) => {
+        e.preventDefault();
+        
+        if (pinsLink.classList.contains("active")) return;
+
+        wrapper.style.top = `-${screenHeight * 2}px`;
+        globeLink.classList.remove("active");
+        countriesLink.classList.remove("active");
+        pinsLink.classList.add("active");
+
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    ready();    
+});
