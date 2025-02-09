4b3a170bd6ef19a372c1c9ffd3db10763efb725c
Jakub Pietrzyk <jakub.pietrzyk01@gmail.com>
2025-02-09 16:22:56 +0100

    Add moving background to any big image

--- scripts/moving_bg.js
+++ scripts/moving_bg.js
@@ -1,5 +1,7 @@
 const body = document.querySelector("body");
-const background = document.querySelector(".background");
+let background = document.querySelector(".background");
+background = background ? background : document.querySelector(".countries-bg");
+background = background ? background : document.querySelector(".trips-bg");
 
 document.addEventListener("mousemove", (e) => {
     if (window.innerWidth > 768) {
@@ -8,7 +10,9 @@ document.addEventListener("mousemove", (e) => {
         const newX = (((elWidth / 2) - e.clientX) * 10 / (elWidth / 2)) - 10;
         const newY = (((elHeight / 2) - e.clientY) * 10 / (elHeight / 2)) - 10;
 
-        background.style.setProperty('--x', newX + "px");
-        background.style.setProperty('--y', newY + "px");
+        if (background) {
+            background.style.setProperty('--x', newX + "px");
+            background.style.setProperty('--y', newY + "px");
+        }
     }
 });
