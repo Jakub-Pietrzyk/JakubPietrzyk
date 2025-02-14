94e63a952a4687c0703837a3669120830a524c18
Jakub <jakub.pietrzyk01@gmail.com>
2025-02-14 10:38:48 +0100

    Add scroll to scroll_button script

--- scripts/scroll_button.js
+++ scripts/scroll_button.js
@@ -1,14 +1,30 @@
 const ready = function() {
-    const scrollButton = document.getElementById("sd_container");
+    const removeWheelListener = () => {
+        window.removeEventListener("wheel", wheelHandler);
+    };
 
+
+    const scrollButton = document.getElementById("sd_container");
     const wrapper = document.getElementById("trips_content_wrapper");
 
-    scrollButton.addEventListener("click", (e) => {
-        console.log("click");
-        
+
+    scrollButton.addEventListener("click", (e) => {        
         e.preventDefault();        
-        wrapper.style.top = `-100vh`;
+        wrapper.classList.add('wrapped-top');
     });
+
+
+    const wheelHandler = (e) => {
+        const delta = e.deltaY;
+
+        if (delta > 50) {
+            wrapper.classList.add('wrapped-top');
+            removeWheelListener();
+        }
+    };
+
+    window.addEventListener("wheel", wheelHandler);
+
 }
 
 document.addEventListener("DOMContentLoaded", function() {
