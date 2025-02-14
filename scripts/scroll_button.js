ad2b0d71a690222bdb6a1805741ee4b8f0c874f8
Jakub <jakub.pietrzyk01@gmail.com>
2025-02-14 10:53:07 +0100

    Add scroll on mobile

--- scripts/scroll_button.js
+++ scripts/scroll_button.js
@@ -1,18 +1,15 @@
 const ready = function() {
-    const removeWheelListener = () => {
-        window.removeEventListener("wheel", wheelHandler);
-    };
-
+    const removeWheelListener = () => { window.removeEventListener("wheel", wheelHandler); };
+    const removeTouchMoveListener = () => { window.removeEventListener("touchmove", touchMoveHandler); };
 
     const scrollButton = document.getElementById("sd_container");
     const wrapper = document.getElementById("trips_content_wrapper");
 
 
-    scrollButton.addEventListener("click", (e) => {        
+    const clickHandler = (e) => {
         e.preventDefault();        
         wrapper.classList.add('wrapped-top');
-    });
-
+    }
 
     const wheelHandler = (e) => {
         const delta = e.deltaY;
@@ -23,8 +20,23 @@ const ready = function() {
         }
     };
 
-    window.addEventListener("wheel", wheelHandler);
+    let startY;
 
+    const touchStartHandler = (e) => { startY = e.touches[0].clientY; };
+
+    const touchMoveHandler = (e) => {
+        const deltaY = startY - e.touches[0].clientY;
+
+        if (deltaY > 50) {
+            wrapper.classList.add('wrapped-top');
+            removeTouchMoveListener();
+        }
+    };
+
+    scrollButton.addEventListener("click", clickHandler);
+    window.addEventListener("wheel", wheelHandler);
+    window.addEventListener("touchstart", touchStartHandler);
+    window.addEventListener("touchmove", touchMoveHandler);
 }
 
 document.addEventListener("DOMContentLoaded", function() {
