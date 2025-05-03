7858f5f37203b272231a4b5020cf8dc53600852e
Jakub <jakub.pietrzyk01@gmail.com>
2025-05-03 14:10:48 +0200

    Reapply "Add on devicemove to water.js"
    This reverts commit 4b0f35ef9df3a0c0ba4b66f2a2358e5b7c233ede.

--- heat_distortion/index/desert.js
+++ heat_distortion/index/desert.js
@@ -118,25 +118,25 @@
 	  }
 	});
 	
-	// window.addEventListener('devicemotion', function (event) {
-	//   if (isTouchDevice) {
-	//     haze.gl.createUniform('2f', 'mouse', curve(smoothX(-event.accelerationIncludingGravity.x / 10)) * 12, curve(smoothY(-event.accelerationIncludingGravity.y / 10)) * 3);
-	//   }
-	// });
-
 	window.addEventListener('devicemotion', function (event) {
-		if (isTouchDevice) {
-		  const adjustedX = -event.accelerationIncludingGravity.x / 80; // Increased divisor for less sensitivity
-		  const adjustedY = (-event.accelerationIncludingGravity.y + 9.8) / 80; // Increased divisor for less sensitivity
+	  if (isTouchDevice) {
+	    haze.gl.createUniform('2f', 'mouse', curve(smoothX(-event.accelerationIncludingGravity.x / 10)) * 12, curve(smoothY(-event.accelerationIncludingGravity.y / 10)) * 3);
+	  }
+	});
+
+	// window.addEventListener('devicemotion', function (event) {
+	// 	if (isTouchDevice) {
+	// 	  const adjustedX = -event.accelerationIncludingGravity.x / 80; // Increased divisor for less sensitivity
+	// 	  const adjustedY = (-event.accelerationIncludingGravity.y + 9.8) / 80; // Increased divisor for less sensitivity
 	  
-		  haze.gl.createUniform(
-			'2f',
-			'mouse',
-			curve(smoothX(adjustedX)) * 1.5, // Reduced multiplier
-			curve(smoothY(adjustedY)) * 0.375 // Reduced multiplier
-		  );
-		}
-	  });
+	// 	  haze.gl.createUniform(
+	// 		'2f',
+	// 		'mouse',
+	// 		curve(smoothX(adjustedX)) * 1.5, // Reduced multiplier
+	// 		curve(smoothY(adjustedY)) * 0.375 // Reduced multiplier
+	// 	  );
+	// 	}
+	//   });
 	
 	haze.gl.createUniform('1i', 'noiseSize', 256);
 	
