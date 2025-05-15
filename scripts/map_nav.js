0bd15ed50387391d637164aaa4f2fccfe990f45a
Jakub Pietrzyk <jakub.pietrzyk01@gmail.com>
2025-05-15 13:21:14 +0200

    Add scroll to maps view

--- scripts/map_nav.js
+++ scripts/map_nav.js
@@ -1,21 +1,45 @@
+const globeLink = document.getElementById("globe_link");
+const countriesLink = document.getElementById("countries_link");
+const pinsLink = document.getElementById("pins_link");
+
+const wrapper = document.getElementById("map_content_wrapper");
+const sideNav = document.getElementById("side_nav");
+
+let isScrolling = false;
+
+moveToCountries = function() {
+    wrapper.style.top = `0px`;
+    globeLink.classList.remove("active");
+    pinsLink.classList.remove("active");
+    countriesLink.classList.add("active");
+    sideNav.classList.add("white");
+    countCountries();
+    emojisDisplay(); 
+}
+
+moveToGlobe = function() {
+    wrapper.style.top = `-100vh`;
+    countriesLink.classList.remove("active");
+    pinsLink.classList.remove("active");
+    globeLink.classList.add("active");     
+    sideNav.classList.remove("white");  
+}
+
+moveToPins = function() {
+    wrapper.style.top = `-200vh`;
+    globeLink.classList.remove("active");
+    countriesLink.classList.remove("active");
+    pinsLink.classList.add("active");
+    sideNav.classList.remove("white");
+}
+
 const ready = function() {
-    const globeLink = document.getElementById("globe_link");
-    const countriesLink = document.getElementById("countries_link");
-    const pinsLink = document.getElementById("pins_link");
-
-    const wrapper = document.getElementById("map_content_wrapper");
-    const sideNav = document.getElementById("side_nav");
-
     globeLink.addEventListener("click", (e) => {
         e.preventDefault();
         
         if (globeLink.classList.contains("active")) return;
 
-        wrapper.style.top = `-100vh`;
-        countriesLink.classList.remove("active");
-        pinsLink.classList.remove("active");
-        globeLink.classList.add("active");     
-        sideNav.classList.remove("white");   
+        moveToGlobe();
     });
 
     countriesLink.addEventListener("click", (e) => {
@@ -23,13 +47,7 @@ const ready = function() {
         
         if (countriesLink.classList.contains("active")) return;
 
-        wrapper.style.top = `0px`;
-        globeLink.classList.remove("active");
-        pinsLink.classList.remove("active");
-        countriesLink.classList.add("active");
-        sideNav.classList.add("white");
-        countCountries();
-        emojisDisplay();
+        moveToCountries();
     });
 
     pinsLink.addEventListener("click", (e) => {
@@ -37,14 +55,93 @@ const ready = function() {
         
         if (pinsLink.classList.contains("active")) return;
 
-        wrapper.style.top = `-200vh`;
-        globeLink.classList.remove("active");
-        countriesLink.classList.remove("active");
-        pinsLink.classList.add("active");
-        sideNav.classList.remove("white");
+        moveToPins();
     });
 }
 
+const scrollReady = function() {
+    const globeLink = document.getElementById("globe_link");
+    const countriesLink = document.getElementById("countries_link");
+    const pinsLink = document.getElementById("pins_link");
+
+    const wheelHandler = (e) => {
+        if (isScrolling) return;
+        const delta = e.deltaY;
+
+        if (delta > 50) {
+            // move down
+
+            if(countriesLink.classList.contains("active")){
+                moveToGlobe();  
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else if(globeLink.classList.contains("active")){
+                moveToPins();
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else {
+                return
+            }
+        } else if (delta < -50) {
+            // move top
+            if(globeLink.classList.contains("active")){
+                moveToCountries();  
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else if(pinsLink.classList.contains("active")){
+                moveToGlobe();
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else {
+                return
+            }
+        }
+    };
+
+    let startY;
+    const touchStartHandler = (e) => { startY = e.touches[0].clientY; };
+
+    const touchMoveHandler = (e) => {
+        if (isScrolling) return;
+        const deltaY = startY - e.touches[0].clientY;
+
+        if (deltaY > 50) {
+            // move down
+
+            if(countriesLink.classList.contains("active")){
+                moveToGlobe();  
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else if(globeLink.classList.contains("active")){
+                moveToPins();
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else {
+                return
+            }
+        } else if (deltaY < -50) {
+            // move top
+            if(globeLink.classList.contains("active")){
+                moveToCountries();  
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else if(pinsLink.classList.contains("active")){
+                moveToGlobe();
+                isScrolling = true;
+                setTimeout(() => { isScrolling = false; }, 1000);
+            } else {
+                return
+            }
+        }
+    };
+
+    window.addEventListener("wheel", wheelHandler);
+    window.addEventListener("touchstart", touchStartHandler);
+    window.addEventListener("touchmove", touchMoveHandler);
+}
+
+
 document.addEventListener("DOMContentLoaded", function() {
-    ready();    
+    ready();
+    scrollReady();    
 });
