9e89025392d95bc2f94ff13bb6c250f28ac7e1cb
Jakub <jakub.pietrzyk01@gmail.com>
2025-06-13 13:45:31 +0200

    Add gyroscope to water pages

--- heat_distortion/trips/water.js
+++ heat_distortion/trips/water.js
@@ -119,16 +119,22 @@
   
 	  window.addEventListener('devicemotion', function (event) {
 		  if (isTouchDevice) {
-			const adjustedX = -event.accelerationIncludingGravity.x / 80; // Increased divisor for less sensitivity
-			const adjustedY = (-event.accelerationIncludingGravity.y + 9.8) / 80; // Increased divisor for less sensitivity
-		
-			haze.gl.createUniform(
-			  '2f',
-			  'mouse',
-			  curve(smoothX(adjustedX)) * 1.5, // Reduced multiplier
-			  curve(smoothY(adjustedY)) * 0.375 // Reduced multiplier
-			);
-		  }
+			let xAcceleration = -event.accelerationIncludingGravity.x;
+			let yAcceleration = -(event.accelerationIncludingGravity.y - 10.0)
+
+			// xAcceleration in [-10, 10]
+			// yAcceleration in [0, 20]
+
+			if(xAcceleration < -4) xAcceleration = -4;
+			if(xAcceleration > 4) xAcceleration = 4;
+
+			if(yAcceleration > 8) yAcceleration = 8;
+
+			const xCurve = curve(smoothX(xAcceleration / 10)) * 8;
+			const yCurve = curve(smoothY(yAcceleration / 10)) * 2;
+
+			haze.gl.createUniform('2f', 'mouse', xCurve, yCurve);
+		}
 		});
 
 	window.addEventListener('resize', updateSize);
